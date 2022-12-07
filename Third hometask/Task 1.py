834b3f467d2bf64ff3b1ca057dc13a439793fd59
skartyman <skartyman@gmail.com>
2022-12-07 23:12:43 +0300

    Готово

--- Third hometask/Task 1.py
+++ Third hometask/Task 1.py
@@ -6,15 +6,15 @@
 left_operand = int(input('Введите левый операнд: '))
 operator = input('Введите рператор: +, -, *, / или ** (возведение в степень)')
 right_operand = int(input('Введите правый операнд: '))
-if operator == "+":
+if operator == "+" :
     print(float(left_operand + right_operand))
-elif operator == "-":
+elif operator == "-" :
     print(float(left_operand - right_operand))
-elif operator == "*":
+elif operator == "*" :
     print(float(left_operand * right_operand))
-elif operator == "/":
+elif operator == "/" :
     print(float(left_operand / right_operand))
-elif operator == "**":
+elif operator == "**" :
     print(float(left_operand ** right_operand))
-else:
+else :
     print('Вы ввели какой-то неизвестный мне оператор, прощайте!')
