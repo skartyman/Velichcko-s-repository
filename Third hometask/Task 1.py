1db8b8ea51b618f2324192bb4aaeb8616c49052d
skartyman <skartyman@gmail.com>
2022-12-07 22:41:53 +0300

    Готово

---
+++ Third hometask/Task 1.py
@@ -0,0 +1,20 @@
+'''
+Написать калькулятор для простых операций(+,-,*,/,**),
+Операнды и оператор вводятся с клавиатуры отдельно(отдельные переменные)
+'''
+
+left_operand = int(input('Введите левый операнд: '))
+operator = input('Введите рператор: +, -, *, / или ** (возведение в степень)')
+right_operand = int(input('Введите правый операнд: '))
+if operator == "+":
+    print(float(left_operand + right_operand))
+elif operator == "-":
+    print(float(left_operand - right_operand))
+elif operator == "*":
+    print(float(left_operand * right_operand))
+elif operator == "/":
+    print(float(left_operand / right_operand))
+elif operator == "**":
+    print(float(left_operand ** right_operand))
+else:
+    print('Вы ввели какой-то неизвестный мне оператор, прощайте!')
