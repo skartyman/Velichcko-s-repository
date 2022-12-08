a70daf293c7486e71ccb24f08e3d501518d955f9
skartyman <skartyman@gmail.com>
2022-12-08 23:35:24 +0300

    Подправил

--- First hometask/Task 3.py
+++ First hometask/Task 3.py
@@ -1,11 +1,2 @@
-print("Escape sequences\n\\a\t Bell (alert)\n\\b \t Backspace\n\\n\t New line\n\ \t Horizontal tab\n\\\ \t Backslash \\\n\\\" \t Double quotation mark \"\n\\' \t Single quotation mark '")
-
-
-print(r'''Escape sequences
-\a	 Bell (alert)
-\b 	 Backspace
-\n	 New line
-\ 	 Horizontal tab
-\\ 	 Backslash \
-\' 	 Single quotation mark '
-\" 	 Double quotation mark "''')
+print("Escape sequences\n\\a\t Bell (alert)\n\\b \t Backspace\n\\n\t New line\n\ \t Horizontal tab\n\\\ \t "
+      "Backslash \\\n\\\" \t Double quotation mark \"\n\\' \t Single quotation mark '")
