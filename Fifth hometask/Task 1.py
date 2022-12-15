e78c1d4d33b50b8e824f35c63ca66038629957cb
skartyman <skartyman@gmail.com>
2022-12-15 18:43:56 +0300

    Название переменной

--- Fifth hometask/Task 1.py
+++ Fifth hometask/Task 1.py
@@ -5,8 +5,8 @@
 '''
 
 from random import randint
-list_1 = [randint(1,300) for i in range(200)] # генерация списка с рандомными двумясотнями значений от 1 до 300
-for n in list_1:
+my_list = [randint(1,300) for i in range(200)] # генерация списка с рандомными двумясотнями значений от 1 до 300
+for n in my_list:
         if n > 100:
             print(n,end=' ')
 
