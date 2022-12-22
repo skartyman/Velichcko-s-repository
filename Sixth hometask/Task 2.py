a8d334c129c1ccf4f4c312d30bffebaf24bc224e
skartyman <skartyman@gmail.com>
2022-12-23 00:22:17 +0300

    Готово

--- Sixth hometask/Task 2.py
+++ Sixth hometask/Task 2.py
@@ -2,9 +2,15 @@
 2. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list стоит на последнем месте.
  Если my_list [1,2,3,4], то new_list [2,3,4,1]
 '''
-my_list = [i for i in range(20)]
+from random import randint
+my_list = [randint(1,5) for i in range(4)]
+print(my_list)
 new_list = []
-for n in my_list[::-1]:
-    new_list.append(n)
+for i in my_list:
+    new_list.append(i)
+new_list.append(new_list.pop(0))
+print(new_list)
+print('Проверка на уникальность списка new_list')
+my_list[1] = my_list[3]
 print(my_list)
 print(new_list)
