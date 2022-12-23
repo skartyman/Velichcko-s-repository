c8927e16de03ba28dac9abe38e06750636467f5f
skartyman <skartyman@gmail.com>
2022-12-23 14:51:03 +0300

    Готово

---
+++ Sixth hometask/Task 3.py
@@ -0,0 +1,9 @@
+'''
+3. Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.[1,2,3,4] -> [2,3,4,1].
+Пересоздавать список нельзя! (используйте метод pop)
+'''
+from random import randint
+my_list = [randint(1,5) for i in range(4)]
+print(my_list)
+my_list.append(my_list.pop(0))
+print(my_list)
