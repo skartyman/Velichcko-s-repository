1ab7cced13ab78a7a702b15020d7fa474dc259bc
skartyman <skartyman@gmail.com>
2023-01-06 21:09:30 +0200

    Готово

--- Seventh hometask/Task 1.py
+++ Seventh hometask/Task 1.py
@@ -1,9 +1,8 @@
 '''
 Дан список словарей persons в формате [{"name": "John", "age": 15}, ... ,{"name": "Jack", "age": 45}]
-    Создать список и поместить туда имя самого молодого человека.
-
+   Создать список и поместить туда самое длинное имя. Если длина имени совпадает - поместить все такие имена.
 '''
-
+print()
 persons = [
     {"name": "Andrew", "age": 56},
     {"name": "John", "age": 54},
@@ -13,7 +12,24 @@ persons = [
     {"name": "Cortney", "age": 45},
     {"name": "Peter", "age": 47}
     ]
-
+print(' Создать список и поместить туда имя самого молодого человека.')
 min_age = min([i['age'] for i in persons])
 min_age_name_list = [i['name'] for i in persons if i['age'] == min_age]
 print(min_age_name_list)
+
+print()
+print('Создать список и поместить туда самое длинное имя. Если длина имени совпадает - поместить все такие имена.')
+
+max_len_name = max([len(i['name']) for i in persons])
+max_len_name_list = [i['name'] for i in persons if len(i['name']) == max_len_name]
+print(max_len_name_list)
+
+print()
+print('Посчитать среднее количество лет всех людей из начального списка.')
+count = 0
+for i in persons:
+    count += 1
+middle_age = sum(i['age'] for i in persons)//count
+print(f'Средний возраст всех людей: {middle_age}')
+
+
--- Seventh hometask/Task 2.py
+++ Seventh hometask/Task 2.py
@@ -1,17 +1,41 @@
 '''
-Дан список словарей persons в формате [{"name": "John", "age": 15}, ... ,{"name": "Jack", "age": 45}]
-   Создать список и поместить туда самое длинное имя. Если длина имени совпадает - поместить все такие имена.
+Даны два словаря my_dict_1 и my_dict_2
 '''
 
-persons = [
-    {"name": "Andrew", "age": 56},
-    {"name": "John", "age": 54},
-    {"name": "Jacob", "age": 68},
-    {"name": "Corry", "age": 46},
-    {"name": "Emily", "age": 45},
-    {"name": "Cortney", "age": 45},
-    {"name": "Peter", "age": 47}
-    ]
-max_len_name = max([len(i['name']) for i in persons])
-max_len_name_list = [i['name'] for i in persons if len(i['name']) == max_len_name]
-print(max_len_name_list)
+
+my_dict_1 = {4: 7, 6: 6, 7: 54, 8: 4, 9: 5 }
+my_dict_2 = {5: 3, 7: 5, 6: 3, 4: 2, 8: 4}
+print()
+print('Создать список из ключей, которые есть в обоих словарях.')
+
+keys_list = list(my_dict_1.keys() & (my_dict_2.keys()))
+print(keys_list)
+print()
+print('Создать список из ключей, которые есть в первом, но нет во втором словаре.')
+
+keys_list_1 = list(my_dict_1.keys())
+keys_list_2 = list(my_dict_2.keys())
+new_list = [i for i in keys_list_1 if not i in keys_list_2]
+print(new_list)
+print()
+print('Создать новый словарь из пар {ключ:значение}, для ключей, которые есть в первом, но нет во втором словаре.')
+
+items_list_1 = list(my_dict_1.items())
+items_list_2 = list(my_dict_2.items())
+new_dict = dict(i for i in items_list_1 if not i in items_list_2)
+print(new_dict)
+print()
+print("Объединить эти два словаря в новый словарь по правилу: \n"
+      " если ключ есть только в одном из двух словарей - поместить пару ключ:значение, \n"
+      " если ключ есть в двух словарях -\n"
+      " поместить пару {ключ: [значение_из_первого_словаря, значение_из_второго_словаря]},\n"
+      "{1:1, 2:2}, {11:11, 2:22} ---> {1:1, 11:11, 2:[2, 22]}')")
+print()
+
+for key in my_dict_2.keys():
+      if key in my_dict_1:
+            my_dict_1[key] = list((my_dict_1[key],my_dict_2[key]))
+      else:
+            my_dict_1.update({key: my_dict_2[key]})
+new_dict = my_dict_1
+print(new_dict)
