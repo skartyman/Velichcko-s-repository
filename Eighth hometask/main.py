ccbc382abd9d622beda7b54d68c3c013fea3763e
skartyman <skartyman@gmail.com>
2023-01-06 22:57:08 +0200

    Готово

--- Eighth hometask/main.py
+++ Eighth hometask/main.py
@@ -1,2 +1,9 @@
-from Functions import turner
-turner(['лицо','картон','песок','нога','мотор'])
+from Functions import *
+my_list = ['лицо','картон','песок','нога','мотор','аренда','акваланг']
+turner(my_list)
+print()
+a_list(my_list)
+print()
+a_random_list(my_list)
+
+
